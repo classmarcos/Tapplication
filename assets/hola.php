7abe48f0839fadbd7d0fbe58ea28b7d925589587
classmarcos <marcos.class07@gmail.com>
2016-02-05 11:12:45 -0400

    Segundo Commit

---
+++ assets/hola.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Ajax CRUD with Bootstrap modals and Datatables</title>
+        <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/datatables/css/dataTables.bootstrap.css" rel="stylesheet">
+        <link href="assets/bootstrap-datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+        <body>
+	<div class="container">
+		<h1 style="font-size:20pt">Ajax CRUD with Bootstrap modals and Datatables with Server side Validation</h1>
+		<h3>Person Data</h3>
+		<br/>
+		<button class="btn btn-success" onclick="add_person()"><i class="glyphicon glyphicon-plus"></i> Add Person</button>
+		<button class="btn btn-default" onclick="reload_table()"> <i class="glyphicon glyphicon-refresh"></i>Reload</button>
+        <br/>
+        <br/>
+        <table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                 <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Gender</th>
+                    <th>Address</th>
+                    <th>Date of Birth</th>
+                    <th style="width:125px;">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+
+            <tfoot>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Gender</th>
+                <th>Address</th>
+                <th>Date of Birth</th>
+                <th>Action</th>
+            </tr>
+            </tfoot>
+
+        </table>
+
+
+	</div>
+
+
+
+
+</body>
+</html>
